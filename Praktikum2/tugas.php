0cf9acb9552a2965b912dcd6ac234a9b5919bd47
Zibad12 <114565895+Zibad12@users.noreply.github.com>
2023-03-02 07:34:55 +0700

    Add files via upload

---
+++ Praktikum2/tugas.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Tugas</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12 mt-4">
+                <form method="POST" action="tugas.php">
+                    <div class="form-group row">
+                        <label for="customer" class="col-4 col-form-label">Customer</label>
+                        <div class="col-8">
+                            <input id="customer" name="customer" placeholder="Nama Pelanggan" type="text"
+                                class="form-control">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-4">Pilih Produk</label>
+                        <div class="col-8">
+                            <div class="custom-control custom-radio custom-control-inline">
+                                <input name="produk" id="produk_0" type="radio" class="custom-control-input"
+                                    value="tv">
+                                <label for="produk_0" class="custom-control-label">TV</label>
+                            </div>
+                            <div class="custom-control custom-radio custom-control-inline">
+                                <input name="produk" id="produk_1" type="radio" class="custom-control-input"
+                                    value="kulkas">
+                                <label for="produk_1" class="custom-control-label">Kulkas</label>
+                            </div>
+                            <div class="custom-control custom-radio custom-control-inline">
+                                <input name="produk" id="produk_2" type="radio" class="custom-control-input"
+                                    value="cuci">
+                                <label for="produk_2" class="custom-control-label">Mesin Cuci</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="jumlah" class="col-4 col-form-label">Jumlah</label>
+                        <div class="col-8">
+                            <input id="jumlah" name="jumlah" placeholder="Jumlah" type="number" class="form-control">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <div class="offset-4 col-8">
+                            <button name="submit" type="submit" class="btn btn-primary">Submit</button>
+                        </div>
+                    </div>
+                </form>
+                
+                <?php
+                $customer = $_POST["customer"]."<br>";
+                $produk = $_POST["produk"]."<br>";
+                $jumlah = $_POST["jumlah"]."<br>";
+                $harga;
+                if($produk == "tv"){
+                    $harga = 4000000;
+                }
+                elseif ($produk == "kulkas"){
+                    $harga = 3100000;
+                }else{
+                    $harga = 3800000;
+                }
+                $total = $harga * $jumlah;
+                  echo "Nama Customer : ".$customer;
+                  echo "Pilih Produk : ".$produk;
+                  echo "Jumlah : ".$jumlah;
+                ?>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
